a5d521e7129d253b08bd118cf81ed2e30ae875d3
Shubham <gharatshubham306@gmail.com>
2026-02-01 21:39:51 +0530

    Fix: track client as normal folder

---
+++ client/src/App.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import { Navbar, Welcome, Transactions, Services, Footer,Loader} from './components'
+
+const App = () => {
+
+  return (
+    <div className='min-h-screen'>
+      <div className='gradient-bg-welcome'>
+        <Navbar/>
+        <Welcome/>
+      </div>
+      <Services/>
+      <Transactions/>
+      <Footer/>
+    </div>
+  )
+}
+
+export default App
